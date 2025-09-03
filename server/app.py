ef7a5e418d478e03b4f4be3b685e2d32848833c2
Chris Roberts <chrisroberts077@gmail.com>
2025-09-02 21:43:10 -0400

    Initial commit: Flask backend, React frontend skeleton, and .gitignore

---
+++ server/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+# Database setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Example model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+# Create DB tables
+with app.app_context():
+    db.create_all()
+
+# Example route
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': u.id, 'name': u.name} for u in users])
+
+if __name__ == '__main__':
+    app.run(debug=True)
